5a09045ffb558b1cfc2741392247846ac6813035
shohily <shohely.haqu@gmail.com>
2015-06-03 09:46:50 -0700

    Result processing

---
+++ ResultProcessingSystemApp/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace ResultProcessingSystemApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.rollTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.compilerTextBox = new System.Windows.Forms.TextBox();
+            this.neumaricalTextBox = new System.Windows.Forms.TextBox();
+            this.softengTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.resultButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.rollTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 149);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(104, 71);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(150, 22);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // rollTextBox
+            // 
+            this.rollTextBox.Location = new System.Drawing.Point(104, 37);
+            this.rollTextBox.Name = "rollTextBox";
+            this.rollTextBox.Size = new System.Drawing.Size(150, 22);
+            this.rollTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox2.Controls.Add(this.compilerTextBox);
+            this.groupBox2.Controls.Add(this.neumaricalTextBox);
+            this.groupBox2.Controls.Add(this.softengTextBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(335, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(333, 149);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Marks";
+            // 
+            // compilerTextBox
+            // 
+            this.compilerTextBox.Location = new System.Drawing.Point(146, 71);
+            this.compilerTextBox.Name = "compilerTextBox";
+            this.compilerTextBox.Size = new System.Drawing.Size(181, 22);
+            this.compilerTextBox.TabIndex = 3;
+            // 
+            // neumaricalTextBox
+            // 
+            this.neumaricalTextBox.Location = new System.Drawing.Point(146, 105);
+            this.neumaricalTextBox.Name = "neumaricalTextBox";
+            this.neumaricalTextBox.Size = new System.Drawing.Size(181, 22);
+            this.neumaricalTextBox.TabIndex = 3;
+            // 
+            // softengTextBox
+            // 
+            this.softengTextBox.Location = new System.Drawing.Point(146, 34);
+            this.softengTextBox.Name = "softengTextBox";
+            this.softengTextBox.Size = new System.Drawing.Size(181, 22);
+            this.softengTextBox.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Neumarical :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Compiler Design :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Software Eng :";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.dataGridView.Location = new System.Drawing.Point(12, 230);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(656, 150);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Roll";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Software Eng Marks";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Compiler Design Marks";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Neumarical Marks";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Soft Eng GPA";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Compiler Design GPA";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Neumarical GPA";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "GPA";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Grade";
+            this.Column10.Name = "Column10";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(92, 186);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(174, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // resultButton
+            // 
+            this.resultButton.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultButton.Location = new System.Drawing.Point(417, 186);
+            this.resultButton.Name = "resultButton";
+            this.resultButton.Size = new System.Drawing.Size(192, 23);
+            this.resultButton.TabIndex = 4;
+            this.resultButton.Text = "Result";
+            this.resultButton.UseVisualStyleBackColor = true;
+            this.resultButton.Click += new System.EventHandler(this.resultButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 392);
+            this.Controls.Add(this.resultButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Result Processing System";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox rollTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox compilerTextBox;
+        private System.Windows.Forms.TextBox neumaricalTextBox;
+        private System.Windows.Forms.TextBox softengTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button resultButton;
+    }
+}
+
---
+++ ResultProcessingSystemApp/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResultProcessingSystemApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string connectionstring = @"server =USER;database=DB_ResultProcessing;integrated security=true";
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            string rollNo,
+                studentName,
+                softwareEngineeringMarks,
+                compilerDesignMarks,
+                numericalAnalysisMarks,
+                softwareEngineeringGrade,
+                compilerDesignGrade,
+                numericalAnalysisGrade;
+            double sumofPoint;
+            rollNo = rollTextBox.Text;
+            studentName = nameTextBox.Text;
+            softwareEngineeringMarks = softengTextBox.Text;
+            compilerDesignMarks = compilerTextBox.Text;
+            numericalAnalysisMarks = neumaricalTextBox.Text;
+            softwareEngineeringGrade = GradeResult(GradePoint(Convert.ToDouble(softwareEngineeringMarks)));
+            compilerDesignGrade = GradeResult(GradePoint(Convert.ToDouble(compilerDesignMarks)));
+            numericalAnalysisGrade = GradeResult(GradePoint(Convert.ToDouble(numericalAnalysisMarks)));
+            sumofPoint = GradePoint(Convert.ToDouble(softwareEngineeringMarks)) +
+                         GradePoint(Convert.ToDouble(compilerDesignMarks)) +
+                         GradePoint(Convert.ToDouble(numericalAnalysisMarks));
+            sumofPoint /= 3;
+            if (sumofPoint < 2)
+                sumofPoint = 0;
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+
+            string query =
+                string.Format(@"insert into Result_Table(Roll,Name,SoftEngMarks,CompilerDesignMarks,NeumaricalMarks,SoftEngGPA,CompilerDesignGPA,NeumaricalGPA,GPA,Grade)
+                                 values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')"
+                ,rollNo,studentName,softwareEngineeringMarks,compilerDesignMarks,numericalAnalysisMarks,softwareEngineeringGrade
+                ,compilerDesignGrade,numericalAnalysisGrade,GradeResult(sumofPoint),Convert.ToString(sumofPoint) );
+            SqlCommand aCommand = new SqlCommand(query, aConnection);
+            MessageBox.Show("Saved Successfully! ");
+            rollTextBox.Text = "";
+            nameTextBox.Text = "";
+            softengTextBox.Text = "";
+            compilerTextBox.Text = "";
+            neumaricalTextBox.Text = "";
+            aCommand.ExecuteNonQuery();
+            aConnection.Close();
+        }
+        public double GradePoint(double marks)
+        {
+            if (marks >= 80)
+                return 4.0;
+            else if (marks >= 75) return 3.75;
+            else if (marks >= 70) return 3.50;
+            else if (marks >= 65) return 3.25;
+            else if (marks >= 60) return 3.0;
+            else if (marks >= 55) return 2.75;
+            else if (marks >= 50) return 2.50;
+            else if (marks >= 45) return 2.25;
+            else if (marks >= 40) return 2.0;
+            else return 0.0;
+        }
+
+        public string GradeResult(double point)
+        {
+            if (point >= 4.0) return "A+";
+            else if (point >= 3.75) return "A";
+            else if (point >= 3.50) return "A-";
+            else if (point >= 3.25) return "B+";
+            else if (point >= 3.00) return "B";
+            else if (point >= 2.75) return "B-";
+            else if (point >= 2.50) return "C+";
+            else if (point >= 2.25) return "C";
+            else if (point >= 2.00) return "D";
+            else return "F";
+        }
+
+        private void resultButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+
+            string tableSql = "SELECT * FROM Result_Table";
+            SqlCommand selectAllTableSqlCommand = new SqlCommand(tableSql, aConnection);
+
+            SqlDataReader tableReader = selectAllTableSqlCommand.ExecuteReader();
+
+            if (tableReader != null)
+                while (tableReader.Read())
+                {
+                    dataGridView.Rows.Add(tableReader[0].ToString(), tableReader[1].ToString(),
+                                                 tableReader[2].ToString(), tableReader[3].ToString(),
+                                                 tableReader[4].ToString(), tableReader[5].ToString(),
+                                                 tableReader[6].ToString(), tableReader[7].ToString(),
+                                                 tableReader[8].ToString(),tableReader[9].ToString());
+                }
+            tableReader.Close();
+            aConnection.Close();
+ 
+        }
+
+    }
+}
